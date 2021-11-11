59e295d41960010cdc9673d9681bab67e92d3a1f
scunn <shanecunn@hotmail.com>
2021-11-11 23:20:22 +0000

    Button Logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
-import Button from './Button';
+import Button from "./Button";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -16,9 +16,18 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+  const { 
+      state, 
+      loading, 
+      error, 
+      searchTerm, 
+      setSearchTerm, 
+      setIsLoadingMore 
+    } = useHomeFetch();
 
   console.log(state);
+
+  if (error) return <div>Something went wrong ...</div>
 
   return (
     <>
@@ -46,7 +55,7 @@ const Home = () => {
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-          <Button text='Load More' />
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
