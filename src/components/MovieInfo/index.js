5235102d4eaea7dac5262856ef294026c9d0ca2e
scunn <shanecunn@hotmail.com>
2021-11-12 20:54:04 +0000

    MovieInfo styled

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -23,7 +23,8 @@ const MovieInfo = ({ movie }) => (
         <h1>{movie.title}</h1>
         <h3>PLOT</h3>
         <p>{movie.overview}</p>
-        <div classname="rating-directors">
+
+        <div className="rating-directors">
           <div>
             <h3>RATING</h3>
             <div className="score">{movie.vote_average}</div>
